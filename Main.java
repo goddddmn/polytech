12af7bf764052c64f8988a8c36b9b976fbc41d54
goddddmn <57621511+goddddmn@users.noreply.github.com>
2019-12-10 17:53:54 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,12 +1,19 @@
+package com.company;
 public class Main {
-    public static void main(String[] args) {
-        R3Vector v1 = new R3Vector(0,0,0);
-        R3Vector v2 = new R3Vector(1,0,0);
-        R3Vector v3 = new R3Vector(1,1,0);
-        R3Vector v4 = new R3Vector(0,1,0);
-        Facet a = new Facet(v1, v2, v3, v4);
-        a.show();
-        a.rotateOX(90);
-        a.show();
+
+    public static void main(String[] args) throws Exception {
+        Xterm.clear();
+        Xterm.setPosition(40,35);
+        int a= Xterm.inputInt();
+        int b= Xterm.inputInt();
+        int c= Xterm.inputInt();
+        Xterm.println(""+a,Xterm.Blue,Xterm.Yellow);
+        Xterm.println(""+b,Xterm.Cyan,Xterm.Red);
+        Xterm.println(""+c,Xterm.Black,Xterm.Green);
+        int d=(a+b+c)/3;
+        Xterm.println(""+d,Xterm.Yellow,Xterm.Black);
+
+
+	// write your code here
     }
 }
